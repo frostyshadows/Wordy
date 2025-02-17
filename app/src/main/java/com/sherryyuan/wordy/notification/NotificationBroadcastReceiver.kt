af22b815c3ad2f9e1b2ce1b7d15cc8fdd15756d7
Sherry Yuan <frostyyshadows@gmail.com>
2025-02-16 19:10:36 -0800

    add update option in notification

--- app/src/main/java/com/sherryyuan/wordy/notification/NotificationBroadcastReceiver.kt
+++ app/src/main/java/com/sherryyuan/wordy/notification/NotificationBroadcastReceiver.kt
@@ -6,6 +6,8 @@ import android.content.Intent
 import androidx.core.app.RemoteInput
 import com.sherryyuan.wordy.notification.NotificationWorker.Companion.ADD_WORD_COUNT_ACTION
 import com.sherryyuan.wordy.notification.NotificationWorker.Companion.KEY_ADD_WORD_COUNT
+import com.sherryyuan.wordy.notification.NotificationWorker.Companion.KEY_UPDATE_WORD_COUNT
+import com.sherryyuan.wordy.notification.NotificationWorker.Companion.UPDATE_WORD_COUNT_ACTION
 import com.sherryyuan.wordy.repositories.EntryRepository
 import com.sherryyuan.wordy.repositories.ProjectRepository
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,28 +20,53 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class NotificationBroadcastReceiver : BroadcastReceiver() {
 
-    @Inject lateinit var projectRepository: ProjectRepository
-    @Inject lateinit var entryRepository: EntryRepository
+    @Inject
+    lateinit var projectRepository: ProjectRepository
+
+    @Inject
+    lateinit var entryRepository: EntryRepository
 
     private val scope = CoroutineScope(Dispatchers.Default)
 
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action == ADD_WORD_COUNT_ACTION) {
-            scope.launch {
-            val wordCountInput = RemoteInput.getResultsFromIntent(intent)?.getCharSequence(KEY_ADD_WORD_COUNT)
-            println("Adding word count: $wordCountInput")
-            val selectedProject = projectRepository.getSelectedProject()
-                .first()
-                val wordCount = wordCountInput?.toString()?.toInt() // TODO: handle non-numeric input
-            if (selectedProject != null && wordCount != null) {
-                entryRepository.insertEntry(
-                    timestamp = System.currentTimeMillis(),
-                    wordCount = wordCount,
-                    projectId = selectedProject.id,
-                    updateWordCountStrategy = EntryRepository.UpdateWordCountStrategy.ADD,
-                )
-            }
+        when (intent.action) {
+            ADD_WORD_COUNT_ACTION -> {
+                scope.launch {
+                    val wordCountInput = RemoteInput.getResultsFromIntent(intent)
+                        ?.getCharSequence(KEY_ADD_WORD_COUNT)
+                    println("Adding word count: $wordCountInput")
+                    val selectedProject = projectRepository.getSelectedProject().first()
+                    val wordCount =
+                        wordCountInput?.toString()?.toInt() // TODO: handle non-numeric input
+                    if (selectedProject != null && wordCount != null) {
+                        entryRepository.insertEntry(
+                            timestamp = System.currentTimeMillis(),
+                            wordCount = wordCount,
+                            projectId = selectedProject.id,
+                            updateWordCountStrategy = EntryRepository.UpdateWordCountStrategy.ADD,
+                        )
+                    }
                 }
+            }
+
+            UPDATE_WORD_COUNT_ACTION -> {
+                scope.launch {
+                    val wordCountInput = RemoteInput.getResultsFromIntent(intent)
+                        ?.getCharSequence(KEY_UPDATE_WORD_COUNT)
+                    println("Updating word count: $wordCountInput")
+                    val selectedProject = projectRepository.getSelectedProject().first()
+                    val wordCount =
+                        wordCountInput?.toString()?.toInt() // TODO: handle non-numeric input
+                    if (selectedProject != null && wordCount != null) {
+                        entryRepository.insertEntry(
+                            timestamp = System.currentTimeMillis(),
+                            wordCount = wordCount,
+                            projectId = selectedProject.id,
+                            updateWordCountStrategy = EntryRepository.UpdateWordCountStrategy.REPLACE,
+                        )
+                    }
+                }
+            }
         }
     }
 }
